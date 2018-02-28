d11eb9129d604f3c3ab68ea7ee62e967dc1b1454
Dmitry Shaido <d.shaido@gmail.com>
2018-02-28 18:34:40 +0300

    task 11

--- src/index.js
+++ src/index.js
@@ -1,26 +1,35 @@
 class SmartCalculator {
   constructor(initialValue) {
-    // your implementation
+    this.expression = initialValue;
   }
 
   add(number) {
-    // your implementation
+    this.expression += ' + ' + number;
+    return this; 
   }
   
   subtract(number) {
-    // your implementation
+    this.expression += ' - ' + number;
+    return this; 
   }
 
   multiply(number) {
-    // your implementation
+    this.expression += ' * ' + number;
+    return this; 
   }
 
   devide(number) {
-    // your implementation
+    this.expression += ' / ' + number;
+    return this; 
   }
 
   pow(number) {
-    // your implementation
+    this.expression += ' ** ' + number;
+    return this; 
+  }
+
+  toString() {
+    return eval(this.expression);
   }
 }
 
